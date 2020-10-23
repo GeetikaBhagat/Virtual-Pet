4266bf34c0398bc1c1dabd31a64c60f9b42359ad
geetika <geetu911@gmail.com>
2020-10-23 05:56:33 +0530

    Changed

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function setup() {
   database = firebase.database();
   dog = createSprite(250, 250);
   dog.addImage(dogImg);
-  dog.scale=0.5
+  dog.scale = 0.5
 
   foodStock = database.ref('Food');
   foodStock.on("value", readStock);
@@ -25,25 +25,37 @@ function setup() {
 function draw() {
 
   background(46, 139, 87);
+  if (foodS !== undefined) {
 
-  if (keyWentDown(UP_ARROW)) {
-    writeStock(foodS);
-    dog.addImage(happyDog);
-    dog.scale = 0.5;
+    if (keyWentDown(UP_ARROW)) {
+      writeStock(foodS);
+      dog.addImage(happyDog);
+    }
+    if (keyWentUp(UP_ARROW)) {
+      dog.addImage(dogImg);
+    }
+    drawSprites();
   }
-  drawSprites();
   //add styles here
   textSize(20);
   fill('red');
   stroke('white');
   text('Press UP arrow to feed the Dog.', 150, 50);
-
+  text("Bottles left: " + foodS, 250, 480);
 }
+
+
 function readStock(data) {
   foodS = data.val();
 }
 
 function writeStock(x) {
+  if (x <= 0) {
+    x = 0;
+  }
+  else {
+    x = x - 1;
+  }
   database.ref('/').update({
     FOOD: x
   })
